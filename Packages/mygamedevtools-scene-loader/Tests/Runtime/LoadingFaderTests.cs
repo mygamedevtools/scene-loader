85f5d6b3303a493aac47495d88a10f66ed38211d
João Borks <joao.borks@gmail.com>
2023-02-07 17:51:49 -0300

    test: Increase loading fader wait delay

--- Packages/mygamedevtools-scene-loader/Tests/Runtime/LoadingFaderTests.cs
+++ Packages/mygamedevtools-scene-loader/Tests/Runtime/LoadingFaderTests.cs
@@ -40,14 +40,14 @@ namespace MyGameDevTools.SceneLoading.Tests
             yield return new WaitForEndOfFrame();
 
             Assert.AreEqual(LoadingState.WaitingToStart, progress.State);
-            yield return new WaitForSeconds(loadingFader.fadeTime + Time.deltaTime);
+            yield return new WaitForSeconds(1.5f * loadingFader.fadeTime);
 
             Assert.AreEqual(LoadingState.Loading, progress.State);
             Assert.AreEqual(1, canvasGroup.alpha);
 
             progress.SetState(LoadingState.TargetSceneLoaded);
 
-            yield return new WaitForSeconds(loadingFader.fadeTime + Time.deltaTime);
+            yield return new WaitForSeconds(1.5f * loadingFader.fadeTime);
 
             Assert.AreEqual(LoadingState.TransitionComplete, progress.State);
             Assert.AreEqual(0, canvasGroup.alpha);
