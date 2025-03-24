1b6cfa8bb29cfe239d6b19408485e299e34e2441
João Borks <joao.borks@gmail.com>
2025-03-24 14:13:36 -0300

    fix: update package export api

--- Assets/Scripts/Editor/PackageExporter.cs
+++ Assets/Scripts/Editor/PackageExporter.cs
@@ -8,13 +8,9 @@ public static class PackageExporter
     public static void ExportPackage()
     {
         string packageName = "com.mygamedevtools.scene-loader";
+        string exportPath = Path.Combine(Application.dataPath, packageName + ".unitypackage");
 
-        string rootGuid = AssetDatabase.AssetPathToGUID("Packages/" + packageName);
-
-        string[] collection = Array.Empty<string>();
-        collection = AssetDatabase.CollectAllChildren(rootGuid, collection);
-
-        PackageUtility.ExportPackage(collection, Path.Combine(Application.dataPath, packageName + ".unitypackage"));
-        Console.WriteLine($"Exported package to: \"{Application.dataPath}/{packageName}.unitypackage\"");
+        AssetDatabase.ExportPackage("Packages/" + packageName, exportPath, ExportPackageOptions.Recurse);
+        Console.WriteLine($"Exported package to: \"{exportPath}\"");
     }
 }
